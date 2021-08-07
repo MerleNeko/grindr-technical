d72489c36cb1082afec34ea244bbeb57ff811fd1
Merle Ferrante <86791968+MerleNeko@users.noreply.github.com>
2021-08-07 14:08:06 -0400

    Add pagination and date

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import {XkcdViewer} from './xkcdViewer/xkcdViewer';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
